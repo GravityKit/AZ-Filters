7272e88d16ac4214d98b00336838666d4e24a29a
Zack Katz <zack@katzwebservices.com>
2014-09-22 12:37:51 -0600

    Almost there...
    * Make sure no bullets and uppercase, also force margins
    * Change to beta version
    * Set search parameter for class
    * Add filter to modify search parameter properly
    * Add input to GV Search Bar input fields
    * Add docBlocks
    * Add logging
    * Only show "Show All" link when a filter is there

--- gravityview-a-z-entry-filter.php
+++ gravityview-a-z-entry-filter.php
@@ -3,7 +3,7 @@
  * Plugin Name: GravityView A-Z Entry Filter
  * Plugin URI: https://gravityview.co
  * Description: Filters your entries via the alphabet.
- * Version: 1.0.0
+ * Version: 1.0-beta
  * Author: Katz Web Services, Inc.
  * Author URI: https://gravityview.co
  * Author Email: admin@gravityview.co
@@ -37,40 +37,46 @@ function gv_extension_az_entry_filtering_load() {
 
 		protected $_title = 'A-Z Entry Filter';
 
-		protected $_version = '1.0.0';
+		protected $_version = '1.0-beta';
 
 		protected $_text_domain = 'gravity-view-az-entry-filter';
 
-		protected $_min_gravityview_version = '1.1.5';
+		protected $_min_gravityview_version = '1.1.6';
 
 		protected $_path = __FILE__;
 
 		public function add_hooks() {
+
 			// Load widget
 			add_action( 'init', array( $this, 'register_az_entry_filter_widget' ) );
+
 			// Print Styles
 			add_action( 'wp_enqueue_scripts', array( $this, 'print_styles' ) );
 
-			define( 'GV_AZ_Entry_Filter_Plugin_Dir_Path', plugin_dir_path( __FILE__ ) );
-			define( 'GV_AZ_Entry_Filter_Plugin_URL', untrailingslashit( plugins_url( '/', __FILE__ ) ) );
+			define( 'GRAVITYVIEW_AZ_FILTER_PATH', plugin_dir_path( __FILE__ ) );
+
+			define( 'GRAVITYVIEW_AZ_FILTER_URL', untrailingslashit( plugins_url( '/', __FILE__ ) ) );
 		}
 
+		/**
+		 * Load the widget files
+		 * @return void
+		 */
 		public function register_az_entry_filter_widget() {
 			// Load the widget class
 			require_once( GRAVITYVIEW_DIR . 'includes/default-widgets.php');
 			// Load widget extension
-			include_once( GV_AZ_Entry_Filter_Plugin_Dir_Path . 'widget/gravityview-a-z-entry-filter-widget.php' );
+			include_once( GRAVITYVIEW_AZ_FILTER_PATH . 'widget/gravityview-a-z-entry-filter-widget.php' );
 		}
 
 		/**
-		* This prints a little bit of CSS to keep the filter
-		* clean on the front-end of the site.
-		* Only called when GravityView is detected on the page.
-		*/
+		 * Print CSS when widget is loaded
+		 * @return void
+		 */
 		function print_styles() {
-			// Need to filter the CSS to load only when required.
 
-			wp_enqueue_style( 'gravityview_az_entry_filter_style', GV_AZ_Entry_Filter_Plugin_URL . '/assets/css/gravityview-az-entry-filter.css' );
+			// Need to filter the CSS to load only when required.
+			wp_enqueue_style( 'gravityview_az_entry_filter', GRAVITYVIEW_AZ_FILTER_URL . '/assets/css/gravityview-az-entry-filter.css' );
 		}
 
 	} // GravityView_A_Z_Entry_Filter_Extension
--- widget/gravityview-a-z-entry-filter-widget.php
+++ widget/gravityview-a-z-entry-filter-widget.php
@@ -1,4 +1,5 @@
 <?php
+
 if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 
 /**
@@ -10,7 +11,20 @@ class GravityView_Widget_A_Z_Entry_Filter extends GravityView_Widget {
 
 	private $letter = false;
 
+	private $letter_parameter;
+
 	function __construct() {
+
+		/**
+		 * Modify the URL parameter used to filter the alphabet by.
+		 *
+		 * For example, you could use `starts_with` as the parameter, and the link would be `/view/example/?starts_with=a` instead of `/view/example/?letter=a`
+		 *
+		 * @var string
+		 */
+		$parameter = apply_filters( 'gravityview_az_filter_parameter', 'letter' );
+
+		$this->letter_parameter = !empty( $parameter ) ? esc_attr( $parameter ) : 'letter';
 
 		/**
 		 * @todo Fix the fetching of the filter fields. Make it ajax.
@@ -46,24 +60,50 @@ class GravityView_Widget_A_Z_Entry_Filter extends GravityView_Widget {
 
 		);
 
+		add_action( 'gravityview_search_widget_fields', array( $this, 'modify_search_widget_fields' ) );
+
 		add_filter( 'gravityview_fe_search_criteria', array( $this, 'filter_entries' ) );
 
 		parent::__construct( $widget_label, $widget_id, $default_values, $settings );
 	}
 
+	/**
+	 * Include the current letter in the search widget so it is included in search results.
+	 *
+	 * Requires GravityView 1.1.7
+	 *
+	 * @param string $search_fields Current HTML field output
+	 * @return string If filter letter exists, adds a hidden input to the fields. Otherwise, returns original fields.
+	 */
+	function modify_search_widget_fields( $search_fields ) {
+
+		if( $this->get_filter_letter() ) {
+			$search_fields .= '<input type="hidden" name="'.$this->letter_parameter.'" value="'.$this->get_filter_letter().'" />';
+		}
+
+		return $search_fields;
+	}
+
+	/**
+	 * Get the currently searched-for letter
+	 *
+	 * @return string|boolean If search being performed, return the letter being filtered by. Otherwise, return false.
+	 */
 	function get_filter_letter() {
 
-		$param = apply_filters( 'gravityview_az_filter_parameter', 'letter' );
-
-		if( !empty( $_GET[ $param ] ) ) {
-			return esc_sql( $_GET[ $param ] );
+		if( !empty( $_GET[ $this->letter_parameter ] ) ) {
+			return esc_sql( $_GET[ $this->letter_parameter ] );
 		}
 
 		return false;
 
 	}
 
-	// This loads the languages we can display the alphabets in.
+	/**
+	 * This loads the languages we can display the alphabets in.
+	 *
+	 * @return array Array of languages available, using the WordPress locale string as the key and the language as the value
+	 */
 	function load_localization() {
 		$local = apply_filters( 'gravityview_az_entry_filter_localization', array(
 			'' => __( 'English', 'gravity-view-az-entry-filter' ),
@@ -78,6 +118,7 @@ class GravityView_Widget_A_Z_Entry_Filter extends GravityView_Widget {
 	}
 
 	function get_filter_fields( $formid ) {
+
 		// Get fields with sub-inputs and no parent
 		$fields = gravityview_get_form_fields( $formid, true, false );
 
@@ -110,51 +151,100 @@ class GravityView_Widget_A_Z_Entry_Filter extends GravityView_Widget {
 		return $output;
 	}
 
+	/**
+	 * Modifies the query performed by GravityView to allow for "starts with" queries.
+	 *
+	 * This is a major hack, but necessary for the plugin to work without core GF changes.
+	 *
+	 * Because Gravity Forms doesn't have a way to search entries using "starts with", we add a placeholder so that when the query comes up in the filter, we can identify it. That placeholder is `[GravityView_Widget_A_Z_Entry_Filter]`. The query is modified from a wildcard "LIKE" search, which allows for anything before or after the query to a query where the search allows a wildcard after the first letter matches.
+	 *
+	 * Here's a basic example for the letter "a":
+	 *
+	 * - Before: `value LIKE %a%` - note the `%` wildcard both before and after. This would match "apple" and "face"
+	 * - After: `value LIKE a%` - The `%` wildcard is now only after the `a`, which would match "apple", not "face"
+	 *
+	 * Here's an actual sample from a real query:
+	 *
+	 * - Before: `WHERE ((value like '%[GRAVITYVIEW_AZ_FILTER_REPLACE]a%'))` - note the wildcard before and after the `GRAVITYVIEW_AZ_FILTER_REPLACE`
+	 * - After: `WHERE ((value like 'a%'))` - Now the wildcard and the placeholder get replaced with just the letter
+	 *
+	 * @param  string $query MySQL query passed to the database
+	 * @return string        If the query contains `GRAVITYVIEW_AZ_FILTER_REPLACE`, it will be a modified query. Otherwise, the original query will be returned.
+	 */
 	function query( $query ) {
 		global $wpdb;
 
+		// Get the letter to filter by. Already sanitized.
 		$letter = $this->get_filter_letter();
 
-		if( false !== $letter && preg_match( '/rg_lead_detail/', $query ) ) {
+		// Make sure the query is the correct, modified query. We don't want to modify any other queries!
+		if( false !== $letter && preg_match( '/rg_lead_detail/', $query ) && preg_match('/GRAVITYVIEW_AZ_FILTER_REPLACE/', $query ) ) {
+
+			do_action( 'gravityview_log_debug', 'GravityView_Widget_A_Z_Entry_Filter[query]: Before filtering by character '.$letter, $query );
 
 			if( in_array( $letter, $this->alphabet ) ) {
 
-				$query = str_replace( "value like '%[REPLACEGV_AZ_FILTER]{$letter}%'", "value like '{$letter}%'", $query );
+				$query = str_replace( "value like '%[GRAVITYVIEW_AZ_FILTER_REPLACE]{$letter}%'", "value like '{$letter}%'", $query );
 
 			} else if( $letter === '0-9' ) {
 
-				$query = str_replace( "value like '%[REPLACEGV_AZ_FILTER]0-9%'", "value REGEXP '[0-9]'", $query );
+				$query = str_replace( "value like '%[GRAVITYVIEW_AZ_FILTER_REPLACE]0-9%'", "value REGEXP '[0-9]'", $query );
 
 			}
 
+			do_action( 'gravityview_log_debug', 'GravityView_Widget_A_Z_Entry_Filter[query]: After filtering by character '.$letter, $query );
 		}
 
 		return $query;
 	}
 
+	/**
+	 * Add search criteria to the GravityView search that fetches entries from Gravity Forms
+	 * @param  array $search_criteria Existing search criteria
+	 * @return array                  Modified search criteria
+	 */
 	function filter_entries( $search_criteria ) {
 		global $gravityview_view;
 
 		$letter = $this->get_filter_letter();
 
 		// No search
-		if( empty( $letter ) ) { return $search_criteria; }
+		if( empty( $letter ) ) {
+
+			do_action( 'gravityview_log_debug', 'GravityView_Widget_A_Z_Entry_Filter[filter_entries]: Not adding search criteria.' );
+
+			return $search_criteria;
+		}
 
 		foreach ($gravityview_view->widgets as $zone => $areas) {
 
+			// Get all the widgets that have ID of `page_letters`
 			$widgets = wp_list_filter( $areas, array('id' => 'page_letters' ));
 
+			// For each widget...
 			foreach ( $widgets as $uniqueid => $widget ) {
 
 				$this->alphabet = $this->get_localized_alphabet( $widget['localization'] );
 
+				/**
+				 * Modifies the query performed by GravityView. As in, the ACTUAL SQL.
+				 *
+				 * @see  GravityView_Widget_A_Z_Entry_Filter::query()
+				 * @hack
+				 */
 				add_filter( 'query', array( $this, 'query') );
 
-				$search_criteria['field_filters'][] = array(
+				$filter = array(
 					'key' => $widget['filter_field'], // The field ID to search e.g. 1.3 is the First Name
-					'value' => '[REPLACEGV_AZ_FILTER]'.$letter, // The value to search
+					'value' => '[GRAVITYVIEW_AZ_FILTER_REPLACE]'.$letter, // The value to search
 					'operator' => 'like', // What to search in. Options: `is`, `isnot`, `>`, `<`, `contains`
 				);
+
+				$search_criteria['field_filters'][] = $filter;
+
+				do_action( 'gravityview_log_debug', 'GravityView_Widget_A_Z_Entry_Filter[filter_entries]: Adding search criteria.', $filter );
+
+				unset( $filter );
 
 			}
 
@@ -163,7 +253,13 @@ class GravityView_Widget_A_Z_Entry_Filter extends GravityView_Widget {
 		return $search_criteria;
 	}
 
-	// Displays the A-Z Filter
+	/**
+	 * Output the HTML for the widget
+	 * @param  array $widget_args Widget settings
+	 * @param  string $content     [description]
+	 * @param  string $context     [description]
+	 * @return [type]              [description]
+	 */
 	public function render_frontend( $widget_args, $content = '', $context = '') {
 		global $gravityview_view;
 
@@ -176,14 +272,11 @@ class GravityView_Widget_A_Z_Entry_Filter extends GravityView_Widget {
 		$localization = $widget_args['localization'];
 		$uppercase = $widget_args['uppercase'];
 
-		/**
-		 * @todo Clean up this - just pass the args directly to the render_alphabet_letters() method.
-		 */
-		$letter_links = array(
+		$args = array(
 			'current_letter' => $this->get_filter_letter()
 		);
 
-		$letter_links = $this->render_alphabet_letters( $letter_links, $localization, $uppercase);
+		$letter_links = $this->render_alphabet_letters( $args, $localization, $uppercase);
 
 		if( !empty( $letter_links ) ) {
 			echo '<div class="gv-widget-letter-links">' . $letter_links . '</div>';
@@ -193,16 +286,18 @@ class GravityView_Widget_A_Z_Entry_Filter extends GravityView_Widget {
 	}
 
 	// Renders the alphabet letters
-	function render_alphabet_letters( $args = '', $charset = '', $uppercase = true ) {
+	function render_alphabet_letters( $args = '', $charset = 'en_US', $uppercase = true ) {
 		global $gravityview_view, $post;
 
 		$form_id = gravityview_get_form_id( $post->ID );
 
 		if( empty($charset) ) { $charset = 'en_US'; } // Loads 'en_US' by default.
 
+		$alphabet_chars = $this->get_localized_alphabet( $charset );
+
 		$defaults = array(
-			'base' => add_query_arg('letter','%#%'),
-			'format' => '&letter=%#%',
+			'base' => add_query_arg( $this->letter_parameter ,'%#%'),
+			'format' => '&'.$this->letter_parameter.'=%#%',
 			'add_args' => array(), //
 			'current_letter' => NULL,
 			'number_character' => _x('#', 'Character representing numbers', 'gravity-view'),
@@ -230,8 +325,6 @@ class GravityView_Widget_A_Z_Entry_Filter extends GravityView_Widget {
 
 		$output .= $args['before_first_letter'];
 
-		$alphabet_chars = $this->get_localized_alphabet( $charset );
-
 		// Add the number character to the beginning of the array
 		array_unshift($alphabet_chars, $args['number_character'] );
 
@@ -243,10 +336,10 @@ class GravityView_Widget_A_Z_Entry_Filter extends GravityView_Widget {
 			// If entries exist then change the link for the letter.
 
 			if( $char === $args['number_character'] ) {
-				$link = add_query_arg( array( 'letter' => '0-9' ) );
+				$link = add_query_arg( array( $this->letter_parameter => '0-9' ) );
 				$title = $args['link_title_number'];
 			} else {
-				$link = add_query_arg( array( 'letter' => $char ) );
+				$link = add_query_arg( array( $this->letter_parameter => $char ) );
 				$title = sprintf( $args['link_title_letter'], $char );
 			}
 
@@ -258,19 +351,30 @@ class GravityView_Widget_A_Z_Entry_Filter extends GravityView_Widget {
 				$classes[] = 'gv-active';
 			}
 
+			// If wanting uppercase letters, give them uppercase letters
 			if( $uppercase ) {
 				$classes[] = 'gv-uppercase';
 			}
 
 			// Outputs the letter to filter the results on click.
-			$output .= '<li class="' . gravityview_sanitize_html_class( $classes ) . '"><a href="' . $link . '" title="'.esc_attr( $title ).'">' . $char . '</a></li>';
+			$output .= '<li class="' . gravityview_sanitize_html_class( $classes ) . '">';
+			$output .= '<a href="' . $link . '" title="'.esc_attr( $title ).'">' . $char . '</a>';
+			$output .= '</li>';
+
 		}
 
 		$output .= $args['after_last_letter'];
 
-		$show_all_text = $uppercase ? $args['show_all_text'] : mb_strtolower( $args['show_all_text'] );
+		/**
+		 * Only show "Show All" link if there's a filter.
+		 */
+		if( $this->get_filter_letter() ) {
 
-		$output .= '<li class="last"><span class="show-all"><a href="' . remove_query_arg('number', remove_query_arg('letter') ) . '">' . esc_html( $show_all_text ) . '</a></span></li>';
+			$show_all_text = $uppercase ? $args['show_all_text'] : mb_strtolower( $args['show_all_text'] );
+
+			$output .= '<li class="last"><span class="show-all"><a href="' . remove_query_arg('number', remove_query_arg( $this->letter_parameter ) ) . '">' . esc_html( $show_all_text ) . '</a></span></li>';
+
+		}
 
 		$output .= '</ul>';
 
@@ -278,8 +382,14 @@ class GravityView_Widget_A_Z_Entry_Filter extends GravityView_Widget {
 	}
 
 
-	// Returns the letters of the alphabets from the localization chosen or set by default.
-	function get_localized_alphabet( $charset ) {
+	/**
+	 * Returns the letters of the alphabets from the localization chosen or set by default.
+	 * @param  string $charset Language code used by WordPress, such as `en_US` and `de_DE`
+	 * @return [type]          [description]
+	 */
+	function get_localized_alphabet( $charset = 'en_US' ) {
+
+		$charset = empty( $charset ) ? 'en_US' : $charset;
 
 		$alphabets = apply_filters( 'gravityview_alphabets', array(
 			'en_US' => array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'),
@@ -291,23 +401,28 @@ class GravityView_Widget_A_Z_Entry_Filter extends GravityView_Widget {
 			'ru_RU' => array('а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я'),
 		) );
 
-		if( isset( $alphabets[ $charset ] ) ) {
-			$alphabet = $alphabets[ $charset ];
-		} else {
-			$alphabet = $alphabets['en_US'];
-		}
+		// If the alphabet exists, use it. Otherwise, use English alphabet.
+		$alphabet = !empty( $alphabets[ $charset ] ) ? $alphabets[ $charset ] : $alphabets['en_US'];
 
 		return $alphabet;
 	}
 
-	// Returns the first letter of the alphabet.
+	/**
+	 * Returns the first letter of the alphabet.
+	 * @param  string $charset WordPress locale string
+	 * @return string          First letter of the alphabet
+	 */
 	function get_first_letter_localized( $charset ) {
 		$alphabet = $this->get_localized_alphabet( $charset );
 
 		return array_shift( $alphabet );
 	}
 
-	// Returns the last letter of the alphabet.
+	/**
+	 * Returns the last letter of the alphabet.
+	 * @param  string $charset WordPress locale string
+	 * @return string          Last letter of the alphabet
+	 */
 	function get_last_letter_localized( $charset ) {
 		$alphabet = $this->get_localized_alphabet( $charset );
 
