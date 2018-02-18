4ec5a86a1577170a39ae9a018907c95796e43186
Gennady Kovshenin <gennady@kovshenin.com>
2018-02-18 21:36:39 +0500

    2.0 Rewrite
    It's here :)

--- gravityview-az-filters.php
+++ gravityview-az-filters.php
@@ -18,105 +18,26 @@ if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
 add_action( 'plugins_loaded', 'gv_extension_az_entry_filtering_load' );
 
 /**
- * Wrapper function to make sure GravityView_Extension has loaded
+ * A simple loader that works with old PHP versions.
+ *
  * @return void
  */
 function gv_extension_az_entry_filtering_load() {
-
-	if ( ! class_exists( 'GravityView_Extension' ) ) {
+	if ( ! class_exists( '\GV\Extension' ) ) {
+		add_action( 'admin_notices', 'gv_extension_az_entry_filtering_noload' );
 		return;
 	}
 
-	/**
-	 * A-Z Entry Filter Widget Extension
-	 *
-	 * @extends GravityView_Extension
-	 */
-	class GravityView_A_Z_Entry_Filter_Extension extends GravityView_Extension {
+	if ( ! class_exists( '\GV\A_Z_Entry_Filter_Extension' ) ) {
+		require dirname( __FILE__ ) . '/gravityview-az-filters-extension.php';
+	}
+}
 
-		protected $_title = 'A-Z Filters';
-		
-		protected $_version = '1.0.8';
-
-		protected $_item_id = 266;
-
-		protected $_text_domain = 'gravityview-az-filters';
-
-		protected $_min_gravityview_version = '1.1.7';
-
-		protected $_path = __FILE__;
-
-		public function add_hooks() {
-
-			// Load widget
-			add_action( 'init', array( $this, 'register_az_entry_filter_widget' ) );
-
-			// Print Styles
-			add_action( 'wp_enqueue_scripts', array( $this, 'print_styles' ) );
-
-			// Admin styles
-			add_action( 'admin_enqueue_scripts', array( $this, 'print_scripts'));
-
-			add_filter( 'gravityview_noconflict_scripts', array( $this, 'register_noconflict') );
-
-			define( 'GRAVITYVIEW_AZ_FILTER_PATH', plugin_dir_path( __FILE__ ) );
-
-			define( 'GRAVITYVIEW_AZ_FILTER_URL', untrailingslashit( plugins_url( '/', __FILE__ ) ) );
-		}
-
-		/**
-		 * Load the widget files
-		 * @return void
-		 */
-		public function register_az_entry_filter_widget() {
-			// Load widget extension
-			include_once( GRAVITYVIEW_AZ_FILTER_PATH . 'widget/gravityview-a-z-entry-filter-widget.php' );
-		}
-
-		/**
-		 * Enable the script in no-conflict mode
-		 *
-		 * @param  array $scripts_or_styles array of scripts to be loaded
-		 * @return array                    Modified array
-		 */
-		function register_noconflict( $scripts_or_styles ) {
-
-			$scripts_or_styles[] = 'gravityview-az-filters';
-
-			return $scripts_or_styles;
-		}
-
-		/**
-		 * Output the script that dynamically loads the fields for the widget settings
-		 * @param  string $hook $pagenow page name
-		 * @return void
-		 */
-		function print_scripts( $hook ) {
-
-			if( !gravityview_is_admin_page($hook, 'single') ) { return; }
-
-			$script_debug = (defined('SCRIPT_DEBUG') && SCRIPT_DEBUG) ? '' : '.min';
-
-			wp_enqueue_script( 'gravityview-az-filters', GRAVITYVIEW_AZ_FILTER_URL . '/assets/js/az-search-widget-admin'.$script_debug.'.js', array('jquery') );
-
-			wp_localize_script( 'gravityview-az-filters', 'gvAZVar', array(
-				'nonce' => wp_create_nonce( 'gravityview_ajaxviews')
-			) );
-
-		}
-
-		/**
-		 * Print CSS on front-end when widget is loaded
-		 * @return void
-		 */
-		function print_styles() {
-
-			// Need to filter the CSS to load only when required.
-			wp_enqueue_style( 'gravityview_az_entry_filter', GRAVITYVIEW_AZ_FILTER_URL . '/assets/css/gravityview-az-filters.css' );
-		}
-
-	} // GravityView_A_Z_Entry_Filter_Extension
-
-	new GravityView_A_Z_Entry_Filter_Extension;
-
+/**
+ * Outputs a loader warning notice.
+ *
+ * @return void
+ */
+function gv_extension_az_entry_filtering_noload() {
+	echo esc_html( 'GravityView A-Z Filters Extension was not loaded. GravityView 2.0 core files not found!' );
 }
